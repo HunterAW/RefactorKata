4bd5801b44252d7a10bf81ef2c1b856099fa6388
Hunter Wright <wright.hunter@gmail.com>
2018-01-24 13:56:00 -0600

    addressing refactoring notes

--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -6,31 +6,26 @@ namespace RefactorKata
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             // TODO: Add connection string to config once config is taught
-
             using (var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;"))
             {
                 var cmd = conn.CreateCommand();
                 cmd.CommandText = "select * from Products";
-
                 var reader = cmd.ExecuteReader();
                 var products = new List<Product>();
 
                 //TODO: Replace with Dapper once Dapper is tought
                 while (reader.Read())
                 {
-                    products.Add(new Product { Name = reader["Name"].ToString() });
+                    products.Add(new Product {Name = reader["Name"].ToString()});
                 }
-
                 foreach (var product in products)
                 {
                     Console.WriteLine(product.Name);
                 }
-
             }
         }
     }
-
 }
